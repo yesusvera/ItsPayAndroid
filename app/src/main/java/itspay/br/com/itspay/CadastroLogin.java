5a5a93d881fdf7396ccc1c009a12ec89cd79077e
yesus <yesusvera@gmail.com>
2016-12-15 15:02:39 -0200

    Captura de número de cartão de crédito pela câmera.

--- app/src/main/java/itspay/br/com/itspay/CadastroLogin.java
+++ app/src/main/java/itspay/br/com/itspay/CadastroLogin.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 import itspay.br.com.mask.MaskEditTextChangedListener;
 
 public class CadastroLogin extends AppCompatActivity {
@@ -28,6 +32,10 @@ public class CadastroLogin extends AppCompatActivity {
     private CheckBox termosDeUso;
 
     private Button criarLogin;
+
+    private ImageButton scanButton;
+
+    private int MY_SCAN_REQUEST_CODE = 100; // arbitrary int
 
 
     @Override
@@ -49,6 +57,7 @@ public class CadastroLogin extends AppCompatActivity {
         cpf.addTextChangedListener(new MaskEditTextChangedListener("###.###.###-##", cpf));
         dataNascimento.addTextChangedListener(new MaskEditTextChangedListener("##/##/####", dataNascimento));
 
+        scanButton = (ImageButton) findViewById(R.id.scanCardButton);
 
         criarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +85,70 @@ public class CadastroLogin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    public void onScanPress(View v) {
+        // This method is set up as an onClick handler in the layout xml
+        // e.g. android:onClick="onScanPress"
+
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_RESTRICT_POSTAL_CODE_TO_NUMERIC_ONLY, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CARDHOLDER_NAME, false); // default: false
+
+        // hides the manual entry button
+        // if set, developers should provide their own manual entry mechanism in the app
+        scanIntent.putExtra(CardIOActivity.EXTRA_SUPPRESS_MANUAL_ENTRY, false); // default: false
+
+        // matches the theme of your application
+        scanIntent.putExtra(CardIOActivity.EXTRA_KEEP_APPLICATION_THEME, true); // default: false
+
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String resultStr;
+//        if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+        if (data != null){
+            CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+            // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+            resultStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+
+            numeroCartao.setText(scanResult.getFormattedCardNumber().replaceAll(" ", "."));
+
+            // Do something with the raw number, e.g.:
+            // myService.setCardNumber( scanResult.cardNumber );r
+
+            if (scanResult.isExpiryValid()) {
+                resultStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+            }
+
+            if (scanResult.cvv != null) {
+                // Never log or display a CVV
+                resultStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+            }
+
+            if (scanResult.postalCode != null) {
+                resultStr += "Postal Code: " + scanResult.postalCode + "\n";
+            }
+
+            if (scanResult.cardholderName != null) {
+                resultStr += "Cardholder Name : " + scanResult.cardholderName + "\n";
+            }
+        } else {
+            resultStr = "Scan was canceled.";
+        }
+
 
     }
 }
