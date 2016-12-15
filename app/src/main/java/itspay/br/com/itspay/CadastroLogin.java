7447ce1d97dcf49ab4ced72cd17715b69c921586
Yesus Castillo <yesus@MacBook-Pro-Yesus.local>
2016-12-15 04:50:57 -0200

    Implementando Login.

--- app/src/main/java/itspay/br/com/itspay/CadastroLogin.java
+++ app/src/main/java/itspay/br/com/itspay/CadastroLogin.java
@@ -1,28 +1,81 @@
 package itspay.br.com.itspay;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import itspay.br.com.mask.MaskEditTextChangedListener;
 
 public class CadastroLogin extends AppCompatActivity {
+
+    private EditText numeroCartao;
+    private EditText dataNascimento;
+    private EditText cpf;
+    private EditText email;
+    private EditText senha;
+
+    private TextView txtViewTermosDeUso;
+
+    private CheckBox termosDeUso;
+
+    private Button criarLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setTitle(R.string.title_activity_cadastro_login);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        numeroCartao = (EditText)findViewById(R.id.numeroCartao);
+        dataNascimento = (EditText)findViewById(R.id.dataNascimento);
+        cpf = (EditText)findViewById(R.id.cpf);
+        email = (EditText)findViewById(R.id.email);
+        senha = (EditText)findViewById(R.id.senha);
+        termosDeUso = (CheckBox)findViewById(R.id.checkTermosDeUso);
+        criarLogin = (Button)findViewById(R.id.buttonCriarLogin);
+        txtViewTermosDeUso = (TextView) findViewById(R.id.txtViewTermosDeUso);
+
+        numeroCartao.addTextChangedListener(new MaskEditTextChangedListener("####.####.####.####", numeroCartao));
+        cpf.addTextChangedListener(new MaskEditTextChangedListener("###.###.###-##", cpf));
+        dataNascimento.addTextChangedListener(new MaskEditTextChangedListener("##/##/####", dataNascimento));
+
+
+        criarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(!termosDeUso.isChecked()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CadastroLogin.this);
+                    builder.setMessage("Você precisa concordar com os termos de uso e políticas de privacidade.")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    termosDeUso.requestFocus();
+                                }
+                            });
+
+                    builder.create().show();
+
+                }
             }
         });
+
+        txtViewTermosDeUso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CadastroLogin.this, TermosDeUsoActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/itspay/br/com/itspay/LoginActivity.java
+++ app/src/main/java/itspay/br/com/itspay/LoginActivity.java
@@ -3,6 +3,7 @@ package itspay.br.com.itspay;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -101,7 +102,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         txtViewCriaLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent cadastroLogin = new Intent(LoginActivity.this, CadastroLogin.class);
+                startActivity(cadastroLogin);
             }
         });
 
