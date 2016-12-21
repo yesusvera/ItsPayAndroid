8dbdcbccfdf14b451b8f0be58a8df3fff7dbaa23
yesus <yesusvera@gmail.com>
2016-12-21 13:54:12 -0200

    - Menu drawer.

--- app/src/main/java/itspay/br/com/activity/MeusCartoesActivity.java
+++ app/src/main/java/itspay/br/com/activity/MeusCartoesActivity.java
@@ -50,7 +50,7 @@ public class MeusCartoesActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Irá abrir o MarketPlace", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
