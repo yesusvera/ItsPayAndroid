868c9dc8bd34849c6dc3bed0f0b983273dfbb539
yesusvera@gmail.com <yesusvera@gmail.com>
2017-01-25 23:09:08 -0200

    - Produtos da Loja Virtual, com imagens e layout ajustado. - CArregamento de imagens dos cartões dinâmico.

--- app/src/main/java/itspay/br/com/fragment/LojaProdutosFragment.java
+++ app/src/main/java/itspay/br/com/fragment/LojaProdutosFragment.java
@@ -46,6 +46,8 @@ public class LojaProdutosFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+
+    
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
